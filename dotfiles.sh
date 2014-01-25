06eb49fcca62075ed7e9b1f449b31f84d6fe2817
Jonas Bengtsson <jonas.b@gmail.com>
2014-01-26 00:17:36 +0100

    Style script logging

--- dotfiles.sh
+++ dotfiles.sh
@@ -3,6 +3,14 @@ set -e -u
 
 THIS_DIR="$(pushd "$(dirname "$0")" > /dev/null; pwd; popd > /dev/null)"
 
+function info() {
+  echo "$1"
+}
+
+function warn() {
+  echo -e "\x1B[31m$1\x1B[0m"
+}
+
 function link_files() {
   local source="$1"
   local target="$2"
@@ -21,28 +29,28 @@ function link_file() {
   if [ -h "$target_file" ]; then
     local symlink="$(readlink "$target_file")"
     if [ "$source_file" = "$symlink" ]; then
-      echo "Skipping $source_file (already linked)"
+      info "Skipping $source_file (already linked)"
     else
-      echo "Skipping $source_file (already symlinked to $symlink)"
+      warn "Skipping $source_file (already symlinked to $symlink)"
     fi
   elif [ -e "$target_file" ]; then
-    echo "Skipping $source_file (target file exists $target_file)"
+    warn "Skipping $source_file (target file exists $target_file)"
   else
-    echo "Linking $source_file -> $target_file"
+    info "Linking $source_file -> $target_file"
     ln -s "$source_file" "$target_file"
   fi
 }
 
-echo "Update source"
+info "Update source"
 (
 cd "$THIS_DIR"
 git submodule update --init --recursive
 )
 
-echo "Link files"
+info "Link files"
 link_files "$THIS_DIR/home/dot" ~ .
 
-echo "Update vim docs"
+info "Update vim docs"
 vim -e -s <<-EOF
 :Helptags
 :quit
