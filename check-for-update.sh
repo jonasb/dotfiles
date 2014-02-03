6fd690167a825587dcfcad7c80fd9e92dff29047
Jonas Bengtsson <jonas.b@gmail.com>
2014-02-03 15:25:47 +0100

    Use lock file to avoid multiple concurrent updates

--- check-for-update.sh
+++ check-for-update.sh
@@ -3,6 +3,7 @@ set -e -u
 
 THIS_DIR="$(pushd "$(dirname "$0")" > /dev/null; pwd; popd > /dev/null)"
 LAST_UPDATE_FILE="$THIS_DIR/.last-update"
+LOCK_FILE="$THIS_DIR/.update.lock"
 epoch_target=7
 
 function current_epoch_days() {
@@ -14,11 +15,16 @@ function update_last_update_file() {
 }
 
 function update() {
-  "$THIS_DIR/update.sh"
-  update_last_update_file
+  [ -f "$LOCK_FILE" ] && echo "Update in progress, abort" && exit 0
+  touch "$LOCK_FILE"
+  success=true
+  "$THIS_DIR/update.sh" || success=false
+  rm "$LOCK_FILE"
+  [ $success == true ] && update_last_update_file
 }
 
 [ -f "$LAST_UPDATE_FILE" ] && source "$LAST_UPDATE_FILE"
+# abort if LAST_UPDATE_FILE doesn't exist or doesn't contain LAST_EPOCH
 [ -z "${LAST_EPOCH+x}" ] && update_last_update_file && exit 0
 
 epoch_diff=$(($(current_epoch_days) - $LAST_EPOCH))
