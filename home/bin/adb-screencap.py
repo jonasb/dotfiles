ffa5b64097e4080b7e8cf524836af9be2eb7609e
Jonas Bengtsson <jonas.b@gmail.com>
2014-10-31 12:15:04 +0100

    adb-screencap.py: clean up

--- home/bin/adb-screencap.py
+++ home/bin/adb-screencap.py
@@ -10,34 +10,52 @@ IMAGEMAGICK_MOGRIFY_CMD='mogrify'
 OPEN_CMD='open' #TODO support other platforms than osx
 DEVICE_TEMP_PATH='/data/local/tmp/s.png'
 
-def ensure_executables_exist(*executables):
-  for executable in executables:
-    if distutils.spawn.find_executable(executable) is None:
-      raise Exception("Couldn't find executable %s" % executable)
+class AdbScreenCapCommand(object):
+  def __init__(self, options, output_path):
+    self.options = options
+    self.output_path = output_path
 
-def adb(*cmd):
-  return subprocess.check_output([ADB_CMD] + list(cmd))
+  def ensure_executables_exist(self, *executables):
+    for executable in executables:
+      if distutils.spawn.find_executable(executable) is None:
+        raise Exception("Couldn't find executable %s" % executable)
 
-def detect_screen_orientation():
-  for line in adb('shell', 'dumpsys', 'input').splitlines():
-    if 'SurfaceOrientation' in line:
-      match = re.search('([\d]+)', line)
-      if match is not None:
-        return int(match.group(0))
-  raise Exception("Couldn't detect screen orientation")
+  def adb(self, *cmd):
+    return subprocess.check_output([ADB_CMD] + list(cmd))
 
-def screen_cap(output_path):
-  adb('shell', 'screencap', '-p', DEVICE_TEMP_PATH)
-  adb('pull', DEVICE_TEMP_PATH, output_path)
-  adb('shell', 'rm', DEVICE_TEMP_PATH)
+  def detect_screen_orientation(self):
+    for line in self.adb('shell', 'dumpsys', 'input').splitlines():
+      if 'SurfaceOrientation' in line:
+        match = re.search('([\d]+)', line)
+        if match is not None:
+          return int(match.group(0))
+    raise Exception("Couldn't detect screen orientation")
 
-def rotate_image(output_path, screen_orientation):
-  if screen_orientation == 0:
-    return
-  subprocess.check_call([IMAGEMAGICK_MOGRIFY_CMD, '-rotate', str(-screen_orientation * 90), output_path])
+  def screen_cap(self):
+    self.adb('shell', 'screencap', '-p', DEVICE_TEMP_PATH)
+    self.adb('pull', DEVICE_TEMP_PATH, self.output_path)
+    self.adb('shell', 'rm', DEVICE_TEMP_PATH)
 
-def open_image(output_path):
-  subprocess.check_call([OPEN_CMD, output_path])
+  def rotate_image(self, screen_orientation):
+    if screen_orientation == 0:
+      return
+    subprocess.check_call([IMAGEMAGICK_MOGRIFY_CMD, '-rotate', str(-screen_orientation * 90), self.output_path])
+
+  def open_image(self):
+    subprocess.check_call([OPEN_CMD, self.output_path])
+
+  def run(self):
+    self.ensure_executables_exist(ADB_CMD, IMAGEMAGICK_MOGRIFY_CMD)
+    if self.options.overwrite:
+      self.ensure_executables_exist(OPEN_CMD)
+
+    screen_orientation = self.detect_screen_orientation()
+    self.screen_cap()
+    self.rotate_image(screen_orientation)
+    print('Captured screen to %s' % self.output_path)
+    if self.options.open:
+      print('Opening...')
+      self.open_image()
 
 def main():
   parser = OptionParser(usage = 'usage: %prog [options] [filename]')
@@ -51,17 +69,7 @@ def main():
   if not options.overwrite and os.path.exists(output_path):
     parser.error('Output file already exists, use -f to overwrite (%s)' % output_path)
 
-  ensure_executables_exist(ADB_CMD, IMAGEMAGICK_MOGRIFY_CMD)
-  if options.open:
-    ensure_executables_exist(OPEN_CMD)
-
-  screen_orientation = detect_screen_orientation()
-  screen_cap(output_path)
-  rotate_image(output_path, screen_orientation)
-  print('Captured screen to %s' % output_path)
-  if options.open:
-    print('Opening...')
-    open_image(output_path)
+  AdbScreenCapCommand(options, output_path).run()
 
 if __name__ == '__main__':
   main()
