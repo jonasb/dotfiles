3f130160cb8033657792c26c261e1be3b5b9d0aa
Jonas Bengtsson <jonas.b@gmail.com>
2019-07-18 00:00:09 +0200

    Enable autostash in update to get stuck less frequently

--- update.sh
+++ update.sh
@@ -7,7 +7,7 @@ THIS_DIR="$(pushd "$(dirname "$0")" > /dev/null; pwd; popd > /dev/null)"
 cd "$THIS_DIR"
 echo "Update source"
 set -x
-git pull
+git pull --rebase --autostash
 git submodule sync
 git submodule update --init --recursive
 
